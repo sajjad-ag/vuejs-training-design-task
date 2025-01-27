51810507010e5220a538153cff5d2be0683f536d
sajjad-ag <dev.sajad.ali@aswar-g.com>
2025-01-27 12:15:03 +0300

    Initial commit

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="p-4"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    code: String,
+    price: Number,
+    image: String,
+    attributes: Object,
+  },
+  filters: {
+    currency(value) {
+      return `$${value.toFixed(2)}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Optional additional styling */
+</style>
