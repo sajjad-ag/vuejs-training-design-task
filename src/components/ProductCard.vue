b623ba7f30c3aa813d58c9a1f7e7a13024a582fc
sajjad-ag <dev.sajad.ali@aswar-g.com>
2025-01-27 21:42:24 +0300

    added mark favorite feature

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -3,52 +3,59 @@
     :class="
       cn(
         'p-4 flex justify-between border-2 border-gray-200  bg-white',
-        props.class
+        product.class
       )
     "
   >
     <div>
       <div class="flex items-center gap-2">
-        <button class="cursor-pointer">
-          <Star class="w-4 h-4 text-gray-200" />
+        <button class="cursor-pointer" @click="markFavorite(product)">
+          <Star
+            class="w-4 h-4 hover:fill-amber-300 hover:text-amber-300"
+            :class="
+              product.favorite
+                ? 'text-amber-300 fill-amber-300'
+                : 'text-gray-200'
+            "
+          />
         </button>
         <span class="font-bold">
-          {{ props.name }}
+          {{ product.name }}
         </span>
       </div>
-      <p>{{ props.code }}</p>
-      <div v-if="props.attributes" class="flex gap-2">
+      <p>{{ product.code }}</p>
+      <div v-if="product.attributes" class="flex gap-2">
         <span
-          v-if="props.attributes.legs"
+          v-if="product.attributes.legs"
           :key="key"
           :class="
             cn(
               'px-2 rounded-full font-semibold text-sm',
-              props.attributes.legs.color_code
+              product.attributes.legs.color_code
             )
           "
         >
-          Legs: {{ props.attributes.legs.text }}
+          Legs: {{ product.attributes.legs.text }}
         </span>
         <span
-          v-if="props.attributes.color"
+          v-if="product.attributes.color"
           :key="key"
           :class="
             cn(
               'px-2 rounded-full font-semibold text-sm',
-              props.attributes.color.color_code
+              product.attributes.color.color_code
             )
           "
         >
-          Color: {{ props.attributes.color.text }}
+          Color: {{ product.attributes.color.text }}
         </span>
       </div>
-      <p>Price: {{ props.price }}</p>
+      <p>Price: {{ product.price }}</p>
     </div>
     <div>
       <img
-        v-if="props.image"
-        :src="props.image"
+        v-if="product.image"
+        :src="product.image"
         alt="Product Image"
         class="w-16 h-16 bg-gray-200"
       />
@@ -59,19 +66,13 @@
 
 <script setup>
 import { cn } from "@/lib/utils";
+import { useProductsStore } from "@/stores/products";
 import { CameraOff, Star } from "lucide-vue-next";
 import { BiCameraFill } from "oh-vue-icons/icons";
 import { CoStar } from "oh-vue-icons/icons";
 const props = defineProps({
-  name: String,
-  code: String,
-  price: Number,
-  image: String,
-  attributes: Object,
-  class: String,
+  product: Object,
 });
-</script>
 
-<style scoped>
-/* Optional additional styling */
-</style>
+const { markFavorite } = useProductsStore();
+</script>
---
+++ src/stores/products.js
@@ -0,0 +1,253 @@
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+
+export const useProductsStore = defineStore("products", () => {
+  const products = ref([
+    {
+      name: "Whiteboard pen",
+      code: "[CONS_0001]",
+      price: "1.20",
+      image: "/images/whiteboard_pen.png",
+      favorite: false,
+    },
+    {
+      name: "Simple pen",
+      code: "[CONS_0002]",
+      price: "1.20",
+      image: "/images/pen.jpg",
+      favorite: false,
+    },
+    {
+      name: "Screw",
+      code: "[CONS_25630]",
+      price: "0.20",
+      image: "/images/screw.jpg",
+      favorite: false,
+    },
+    {
+      name: "Bolt",
+      code: "[CONS_89957]",
+      price: "0.50",
+      image: "/images/bolt.jpg",
+      favorite: false,
+    },
+    {
+      name: "Customizable Desk",
+      code: "[DESK0005]",
+      price: "750.00",
+      attributes: {
+        color: {
+          text: "White",
+          color_code: "bg-[#8DDED5]",
+        },
+        legs: {
+          text: "Custom",
+          color_code: "bg-[#B9E9BF]",
+        },
+      },
+      image: "/images/desk.png",
+      favorite: false,
+    },
+    {
+      name: "Customizable Desk",
+      code: "[DESK0006]",
+      attributes: {
+        color: {
+          text: "Black",
+          color_code: "bg-[#E5DCF9]",
+        },
+        legs: {
+          text: "Custom",
+          color_code: "bg-[#B9E9BF]",
+        },
+      },
+      price: "750.00",
+      image: "/images/desk.png",
+      favorite: false,
+    },
+    {
+      name: "Stool",
+      code: "[D_0045_B]",
+      attributes: {
+        color: {
+          text: "Dark Blue",
+          color_code: "bg-[#8DDED5]",
+        },
+      },
+      price: "500.00",
+      favorite: false,
+    },
+    {
+      name: "Stool",
+      code: "[D_0045_G]",
+      attributes: {
+        color: {
+          text: "Green",
+          color_code: "bg-[#E5DCF9]",
+        },
+      },
+      price: "500.00",
+      favorite: false,
+    },
+    {
+      name: "Stool",
+      code: "[D_0045_GR]",
+      attributes: {
+        color: "Grey",
+      },
+      price: "500.00",
+      favorite: false,
+    },
+    {
+      name: "Corner Desk Right Sit",
+      code: "[E-COM06]",
+      price: "147.00",
+      image: "/images/corner_desk.jpg",
+      favorite: false,
+    },
+    {
+      name: "Large Cabinet",
+      code: "[E-COM07]",
+      price: "320.00",
+      image: "/images/large_cabinet.jpg",
+      favorite: false,
+    },
+    {
+      name: "Storage Box",
+      code: "[E-COM08]",
+      price: "15.80",
+      image: "/images/storage_box.jpg",
+      favorite: false,
+    },
+    {
+      name: "Large Desk",
+      code: "[E-COM09]",
+      price: "1,799.00",
+      image: "/images/customizable_desk.png",
+      favorite: false,
+    },
+    {
+      name: "Pedal Bin",
+      code: "[E-COM10]",
+      price: "47.00",
+      image: "/images/pedal_bin.jpg",
+      favorite: false,
+    },
+    {
+      name: "Cabinet with Doors",
+      code: "[E-COM11]",
+      price: "140.00",
+      image: "/images/cabinet_with_doors.jpg",
+      favorite: false,
+    },
+    {
+      name: "Conference Chair",
+      code: "[E-COM12]",
+      attributes: {
+        legs: {
+          text: "Steel",
+          color_code: "bg-[#BFD7F4]",
+        },
+      },
+      price: "33.00",
+      image: "/images/conference_chair.jpg",
+      favorite: false,
+    },
+    {
+      name: "Conference Chair",
+      code: "[E-COM13]",
+      attributes: {
+        legs: {
+          text: "Aluminum",
+          color_code: "bg-[#98A5EF]",
+        },
+      },
+      price: "39.40",
+      image: "/images/conference_chair.jpg",
+      favorite: false,
+    },
+    {
+      name: "Customized Cabinet (Metric)",
+      code: "[E-COM98]",
+      price: "210.00",
+      image: "/images/cabinet_with_doors.jpg",
+      favorite: false,
+    },
+    {
+      name: "Customized Cabinet (USA)",
+      code: "[E-COM99]",
+      price: "200.00",
+      image: "/images/cabinet_with_doors.jpg",
+      favorite: false,
+    },
+    {
+      name: "Expenses",
+      code: "[EXP_GEN]",
+      price: "1.00",
+      image: "/images/expenses.png",
+      favorite: false,
+    },
+    {
+      name: "Desk Organizer",
+      code: "[FURN_0001]",
+      price: "5.10",
+      image: "/images/desk_organizer.jpg",
+      favorite: false,
+    },
+    {
+      name: "Desk Pad",
+      code: "[FURN_0002]",
+      price: "1.98",
+      image: "/images/desk_pad.jpg",
+      favorite: false,
+    },
+    {
+      name: "LED Lamp",
+      code: "[FURN_0003]",
+      price: "0.90",
+      image: "/images/led_lamp.jpg",
+      favorite: false,
+    },
+    {
+      name: "Letter Tray",
+      code: "[FURN_0004]",
+      price: "4.80",
+      image: "/images/letter_tray.jpg",
+      favorite: false,
+    },
+    {
+      name: "Magnetic Board",
+      code: "[FURN_0005]",
+      price: "1.98",
+      image: "/images/magnetic_board.jpg",
+      favorite: false,
+    },
+    {
+      name: "Monitor Stand",
+      code: "[FURN_0006]",
+      price: "3.19",
+      image: "/images/monitor_stand.jpg",
+      favorite: false,
+    },
+    {
+      name: "Newspaper Rack",
+      code: "[FURN_0007]",
+      price: "1.28",
+      image: "/images/newspaper_rack.jpg",
+      favorite: false,
+    },
+    {
+      name: "Small Shelf",
+      code: "[FURN_0008]",
+      price: "2.83",
+      image: "/images/small_shelf.jpg",
+      favorite: false,
+    },
+  ]);
+
+  const markFavorite = (product) => {
+    product.favorite = !product.favorite;
+  };
+
+  return { markFavorite, products };
+});
